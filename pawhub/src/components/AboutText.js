be9201e32439b046489ff8a4f0c37a252ec081bc
Jesus Carballosa <jesuscar1997@knights.ucf.edu>
2023-07-08 16:49:53 -0400

    finalizing  about us page fr fr

---
+++ pawhub/src/components/AboutText.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../pages/Pages.css";
+
+
+function AboutText() {
+    return (
+        <div>
+            <p className="Paragraph">   PawHub is a pet website where pets 
+            can create profiles, login, and sign up for a unique social 
+            media platform. It offers a space for pets to connect, share 
+            stories, and engage with fellow furry friends. From adorable pictures
+             to daily adventures, PawHub fosters social interaction and strengthens 
+             the bond between pets in the digital realm.
+            </p>
+
+        </div>
+
+    )
+}
+
+
+export default AboutText;
