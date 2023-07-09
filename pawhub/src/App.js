5d292c93bfab435a33e97d7c3c00137d004019ec
LargeProjectTeamMember <LargeProjectTeamMember@github.com>
2023-07-09 19:13:05 +0000

    api testing

--- pawhub/src/App.js
+++ pawhub/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HomePage from "./pages/HomePage";
 import NoPage from "./pages/NoPage";
 import AboutPage from "./pages/AboutPage";
@@ -8,24 +8,34 @@ import LandingPage from "./pages/LandingPage";
 import GroupChat from "./pages/GroupChat";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/user')
+      .then(response => response.json())
+      .then(users => {
+        setData(users);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
-  <div>
-  <BrowserRouter>
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/landingPage" element={<LandingPage />} />
-      <Route path="*" element={<NoPage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/chat" element={<GroupChat />} />
-    </Routes>
-  </BrowserRouter>
-
-
-  </div> )
-  
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/landingPage" element={<LandingPage />} />
+          <Route path="*" element={<NoPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/chat" element={<GroupChat />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
-
