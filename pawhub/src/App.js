d72b15a805ff1f5f8ed62cb7335c1193534837f6
Parker McLeod <parkercmcleod@gmail.com>
2023-07-12 00:09:37 -0400

    properly implemented GET requests

--- pawhub/src/App.js
+++ pawhub/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import HomePage from "./pages/HomePage";
 import NoPage from "./pages/NoPage";
 import AboutPage from "./pages/AboutPage";
@@ -8,32 +9,45 @@ import LandingPage from "./pages/LandingPage";
 import GroupChat from "./pages/GroupChat";
 
 function App() {
-  const [data, setData] = useState([]);
+  const [apiData, setApiData] = useState([]);
 
   useEffect(() => {
-    fetch('/api/user')
-      .then(response => response.json())
-      .then(users => {
-        setData(users);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
+    // Define an async function to fetch the data
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/api');
+        console.error(response);
+        setApiData(response.data);
+      } catch (error) {
+        console.error("RESPONSE2");
+        console.error('Failed to fetch data', error);
+      }
+    };
 
+    fetchData(); // Call the fetchData function when the component mounts
+  }, []); // The empty dependency array ensures that the effect runs only once
+  
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<HomePage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/landingPage" element={<LandingPage />} />
-          <Route path="*" element={<NoPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/chat" element={<GroupChat />} />
-        </Routes>
-      </BrowserRouter>
+      {/* Example render of api data */}
+      {/* <div>
+        {data.map((item) => (
+          <div key={item._id}>{item.name}</div>
+        ))}
+      </div> */}
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/landingPage" element={<LandingPage />} />
+            <Route path="*" element={<NoPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/chat" element={<GroupChat />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
