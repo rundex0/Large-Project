a8b509f90e40038b3a43e9819618ecbc3f853e3b
Nathan <nathan.carney@knights.ucf.edu>
2023-07-20 14:02:20 -0400

    resized the profile/login/signup cards to not be so big. Added pic to NoPage Page

--- pawhub/src/pages/NoPage.js
+++ pawhub/src/pages/NoPage.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navigation from "../components/Navigation";
-
+import NotFoundPic from "../images/maxresdefault.jpg"; // Import the image you want to use for the background
 
 function NoPage() {
-    return (
-        <div>
-            <Navigation />
+  useEffect(() => {
+    document.body.style.backgroundImage = `url(${NotFoundPic})`;
+    document.body.style.backgroundSize = "50%";
+    document.body.style.backgroundPosition = "center";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundAttachment = "fixed";
 
-            <h2 style={{ paddingTop: "100px" }}>
-                Error 404: Not Found
-            </h2>
+    return () => {
+      document.body.style.backgroundImage = null;
+      document.body.style.backgroundSize = null;
+      document.body.style.backgroundPosition = null;
+      document.body.style.backgroundRepeat = null;
+      document.body.style.backgroundAttachment = null;
+    };
+  }, []); // The empty array [] as the second argument makes the effect run only once (on component mount)
 
+  return (
+    <div>
+      <Navigation />
 
-        </div>)
-
+      <h2 style={{ paddingTop: "100px", color: "black", fontSize: "40px", textAlign: "center" }}>
+        Error 404: Not Found
+      </h2>
+    </div>
+  );
 }
 
 export default NoPage;
-
