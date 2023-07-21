a463c6af7f32bc3b46a89d280cead08ffb2b0da2
Parker McLeod <parkercmcleod@gmail.com>
2023-07-21 02:44:47 -0400

    all posts and users api finished

--- pawhub/src/Api.js
+++ pawhub/src/Api.js
@@ -36,13 +36,20 @@ async function run() {
   await client.connect();
 
   const databaseUsers = client.db("users");
-  const collectionUsers = "users";
 
+  const collectionUsers = "users";
   const users = databaseUsers.collection(collectionUsers);
 
   const collectionCurrentUserIDIncrement = "currentUserIDIncrement";
-
   const currentUserIDIncrement = databaseUsers.collection(collectionCurrentUserIDIncrement);
+
+  const databasePosts = client.db("posts");
+
+  const collectionPosts = "posts";
+  const posts = databasePosts.collection(collectionPosts);
+
+  const collectionCurrentPostIDIncrement = "currentPostIDIncrement";
+  const currentPostIDIncrement = databasePosts.collection(collectionCurrentPostIDIncrement);
 
   // API to search for users by query
   app.get("/searchUsersReturnUsers", async (req, res) => {
@@ -165,6 +172,134 @@ async function run() {
 
       // If the document was found and updated, send the new value
       return result.value.currentUserIDIncrement;
+    } catch (err) {
+      // If an error occurred, send a 500 response
+      res.status(500).send(err);
+    }
+  };
+
+
+  // API to search for posts by query
+  app.get("/searchPostsReturnPosts", async (req, res) => {
+    try {
+      // Extract the query parameters from the request
+      const query = req.query;
+
+      // Perform the search
+      const documents = await posts.find(query).toArray();
+
+      if (documents.length > 0) {
+        res.json(documents);
+      } else {
+        res.status(404).send("No documents found");
+      }
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+
+  // API to search for posts IDs by query
+  app.get("/searchPostsReturnIDs", async (req, res) => {
+    try {
+      // Extract the query parameters from the request
+      const query = req.query;
+
+      // Perform the search
+      const documents = await posts.find(query).toArray();
+
+      if (documents.length > 0) {
+        // Extract the IDs from the documents
+        const documentIds = documents.map((doc) => doc._id);
+
+        res.json(documentIds);
+      } else {
+        res.status(404).send("No documents found");
+      }
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+
+  // API to create a new post
+  app.post("/addNewPost", async (req, res) => {
+    try {
+      let newPostID;
+      let newPost = req.body;
+      try {
+        newPostID = await incrementCurrentPostIDIncrement();
+      } catch (err) {
+        console.error("An error occurred:", err);
+      }
+      const currentDate = new Date();
+      const newDateCreated = currentDate.toISOString();
+      newPost = {
+        postID: newPostID,
+        dateCreated: newDateCreated,
+        ...newPost,
+      };
+      const result = await posts.insertOne(newPost);
+      res.json({message: "Data inserted successfully", result});
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+
+  // API to update posts
+  app.put("/updateMatchingPosts", async (req, res) => {
+    try {
+      const listIDs = req.body.listIDs;
+      const updatedPost = req.body.updatedPost;
+      const objectIDs = listIDs.map(id => new ObjectId(id));
+      const filter = { _id: { $in: objectIDs } };
+
+      const result = await posts.updateMany(filter, { $set: updatedPost });
+
+      const modifiedCount = result.modifiedCount;
+
+      if (modifiedCount === 0) {
+        res.status(404).send('No documents were found for the provided IDs');
+      } else {
+        res.json({message: 'Data updated successfully', modifiedCount: modifiedCount});
+      }
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+
+  // API to delete posts
+  app.delete("/deleteMatchingPosts", async (req, res) => {
+    try {
+      // Extract the query parameters from the request
+      const query = req.query;
+
+      const result = await posts.deleteMany(query);
+      if (result.deletedCount === 0) {
+        res.status(404).send("No such document exists");
+      } else {
+        res.json({message: "Data deleted successfully", deletedCount: result.deletedCount});
+      }
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+
+  // API to increment and return currentPostIDIncrement
+  async function incrementCurrentPostIDIncrement() {
+    try {
+      // Find the document, increment the value and return the updated document
+      const result = await currentPostIDIncrement.findOneAndUpdate(
+        {},  // Filter - empty to match all documents in the collection
+        { $inc: { currentPostIDIncrement: 1 } },  // Update - increment the value by 1
+        { returnOriginal: false }  // Options - return the updated document
+      );
+
+      // If the document was not found, send a 404 response
+      if (!result.value) {
+        return res.status(404).send('No document found');
+      }
+
+      // If the document was found and updated, send the new value
+      return result.value.currentPostIDIncrement;
     } catch (err) {
       // If an error occurred, send a 500 response
       res.status(500).send(err);
--- pawhub/src/App.js
+++ pawhub/src/App.js
@@ -16,7 +16,7 @@ function App() {
   // API IMPLEMENTATION, NOT FOR NATE OR JESUS
   const searchUsersReturnUsers = async (query) => {
     try {
-      const response = await axios.get('http://localhost:3001/searchUsersReturnUsers', {
+      let response = await axios.get('http://localhost:3001/searchUsersReturnUsers', {
         params: query
       });
       setApiData(response.data); // uses the data and rerenders relevant changes
@@ -28,7 +28,7 @@ function App() {
   // API IMPLEMENTATION, NOT FOR NATE OR JESUS
   const searchUsersReturnIDs = async (query) => {
     try {
-      const response = await axios.get('http://localhost:3001/searchUsersReturnIDs', {
+      let response = await axios.get('http://localhost:3001/searchUsersReturnIDs', {
         params: query
       });
       return response.data;
@@ -40,7 +40,7 @@ function App() {
   // API IMPLEMENTATION, NOT FOR NATE OR JESUS
   const addNewUser = async (newUser) => {
     try {
-      const response = await axios.post('http://localhost:3001/addNewUser', newUser);
+      let response = await axios.post('http://localhost:3001/addNewUser', newUser);
       setApiData(response.data); // uses the data and rerenders relevant changes
     } catch (error) {
       console.error('Failed to post data', error);
@@ -70,6 +70,69 @@ function App() {
   const deleteMatchingUsers = async (query) => {
     try {
       await axios.delete(`http://localhost:3001/deleteMatchingUsers`);
+      setApiData(response.data); // uses the data and rerenders relevant changes
+    } catch (error) {
+      console.error('Failed to delete data', error);
+    }
+  };
+
+  // API IMPLEMENTATION, NOT FOR NATE OR JESUS
+  const searchPostsReturnPosts = async (query) => {
+    try {
+      let response = await axios.get('http://localhost:3001/searchPostsReturnPosts', {
+        params: query
+      });
+      setApiData(response.data); // uses the data and rerenders relevant changes
+    } catch (error) {
+      console.error('Failed to search data', error);
+    }
+  };
+
+  // API IMPLEMENTATION, NOT FOR NATE OR JESUS
+  const searchPostsReturnIDs = async (query) => {
+    try {
+      let response = await axios.get('http://localhost:3001/searchPostsReturnIDs', {
+        params: query
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Failed to search data', error);
+    }
+  };
+
+  // API IMPLEMENTATION, NOT FOR NATE OR JESUS
+  const addNewPost = async (newPost) => {
+    try {
+      let response = await axios.post('http://localhost:3001/addNewPost', newPost);
+      setApiData(response.data); // uses the data and rerenders relevant changes
+    } catch (error) {
+      console.error('Failed to post data', error);
+    }
+  };
+
+  // API IMPLEMENTATION, NOT FOR NATE OR JESUS
+  const updateAllMatchingPosts = async (listIDsPromise, updatedPost) => {
+    try {
+      const listIDs = await listIDsPromise;
+      const { data } = await axios.put("http://localhost:3001/updateMatchingPosts", {
+        listIDs,
+        updatedPost,
+      });
+      if (data.status === 200 && data.message === "Data updated successfully") {
+        setApiData((apiData) =>
+          apiData.map((post) => (listIDs.includes(post._id) ? updatedPost : post))
+        );
+      }
+      setApiData(response.data); // uses the data and rerenders relevant changes
+    } catch (error) {
+      console.error("Failed to update data:", error.message);
+    }
+  };
+
+  // API IMPLEMENTATION, NOT FOR NATE OR JESUS
+  const deleteMatchingPosts = async (query) => {
+    try {
+      await axios.delete(`http://localhost:3001/deleteMatchingPosts`);
       setApiData(response.data); // uses the data and rerenders relevant changes
     } catch (error) {
       console.error('Failed to delete data', error);
@@ -110,11 +173,45 @@ function App() {
       await updateAllMatchingUsers(userSearchResultsIDs, updatedUserExample);
 
       // delete all users matching a query
-      query = { username: "IPlayFootball" };
-      await deleteMatchingUsers({ username: "IPlayFootballUPDATED" });
+      query = { postname: "IPlayFootballUPDATED" };
+      await deleteMatchingUsers(query);
+    };
+
+    // declare function examplePostsAPIFunctionality
+    const examplePostsAPIFunctionality = async () => {
+      // create a new post json item
+      const newPostExample = {
+        numLikes: 8,
+        text: "Hello World!",
+        photo: "https://example.com/profile.jpg",
+        userID: 42
+      };
+      // add the new post to database
+      await addNewPost(newPostExample);
+
+      // search for posts matching query and return them
+      let query = { numLikes: 8 };
+      let postSearchResults = await searchPostsReturnPosts(query);
+
+      // search for posts matching query and return their IDs
+      let postSearchResultsIDs = searchPostsReturnIDs(query);
+
+      // set all posts maching IDs from search results above to be this new post
+      const updatedPostExample = {
+        numLikes: 10000,
+        text: "Hello World!UPDATED",
+        photo: "https://example.com/profile.jpgUPDATED",
+        userID: 42000
+      };
+      await updateAllMatchingPosts(postSearchResultsIDs, updatedPostExample);
+
+      // delete all posts matching a query
+      query = { numLikes: 10000 };
+      await deleteMatchingPosts(query);
     };
 
     exampleUsersAPIFunctionality();
+    examplePostsAPIFunctionality();
   }, []);
 
   return (
