0811da87e284249c47e2d4c82e3746c9c2b2ce7e
JesusCarb <JesusCar1997@knights.ucf.edu>
2023-07-23 15:32:06 -0400

    modified donation page and links in home

--- pawhub/src/components/Links.js
+++ pawhub/src/components/Links.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import "../pages/Pages.css";
+import image from "../images/github.png"
 
 function Links() {
   return (
 
     <div className="TitleHolderLinks">
+      <img src= {image} />
+      
       <h1><a href="https://github.com/ParkerCMcLeod/Large-Project" >Website GitHub</a>
         <a href="https://github.com/rundex0/Large-Project-App" >App GitHub</a>
       </h1>
-      <img src={process.env.PUBLIC_URL + "/images/"} alt="GitHub Pic" />
     </div>
   )
 }
